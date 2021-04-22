3ef83188bee3013a336c774adf1432ef6ac63536
Yessica Cabrera Ferrer <yessicaferrer21@gmail.com>
2021-04-22 18:46:24 +0100

    UPDATE TAREA 5.5

--- trim3/Tareas/Unidad5/Tarea5.5.sql
+++ trim3/Tareas/Unidad5/Tarea5.5.sql
@@ -34,52 +34,73 @@
     INSERT INTO SOCIOS CHAR(23333444), VARCHAR('Ana Lopez'), VARCHAR('Caseros 984');
     INSERT INTO SOCIOS CHAR(24333444), VARCHAR('Marcelo Morales'), VARCHAR('Sucre 356');
     INSERT INTO SOCIOS CHAR(25333444), VARCHAR('Susana Juarez'), VARCHAR('Sarmiento 723');
-    INSERT INTO DEPORTES TINYINT(1), VARCHAR
+    INSERT INTO DEPORTES TINYINT(1), VARCHAR('tenis'), VARCHAR('Tadeo Torres');
+    INSERT INTO DEPORTES TINYINT(2), VARCHAR('natacion'), VARCHAR('Natalio Nores');
+    INSERT INTO DEPORTES TINYINT(3), VARCHAR('basquet'), VARCHAR('Bautista Pereyra');
+    INSERT INTO DEPORTES TINYINT(4), VARCHAR('paddel'), VARCHAR('Bautista Pereyra');
+    INSERT INTO INSCRITOS CHAR(22333444), VARCHAR(1), YEAR(2015), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(22333444), VARCHAR(1), YEAR(2016), CHAR(n);
+    INSERT INTO INSCRITOS CHAR(22333444), VARCHAR(2), YEAR(2015), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(23333444), VARCHAR(1), YEAR(2015), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(23333444), VARCHAR(1), YEAR(2016), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(23333444), VARCHAR(2), YEAR(2016), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(24333444), VARCHAR(1), YEAR(2016), CHAR(s);
+    INSERT INTO INSCRITOS CHAR(24333444), VARCHAR(3), YEAR(2016), CHAR(N);
 
 -- 4-Se desea saber la cantidad de alumnos inscritos en cada deporte cada año, sin considerar los deportes para los cuales no hay inscritos:
- 
+    SELECT D.NOMBRE,I.AO,COUNT(I.CODIGODEPORTE) AS CANTIDAD FROM DEPORTES 00000000000000000000000000000000000000000000000000000AS D JOIN INSCRITOS AS I ON D.CODIGO=I.CODIGODEPORTE GROUP BY D.NOMBRE,I.AO;
 
 -- 5-El club quiere almacenar esa información en una tabla llamada `inscritospordeporteporaño`. Elimine la tabla, si existe.
-
+    DROP TABLE IF EXISTS INSCRITOSPORDEPORTEPORANIO;
 
 -- 6-Cree la tabla con la siguiente estructura:
- 
-
+    CREATE TABLE INSCRITOSPORDEPORTEPORANIO(
+        DEPORTE VARCHAR(15);
+        AO YEAR;
+        CANTIDAD TINYINT UNSIGNED;
+    )
 
 -- 7-Ingrese registros en la tabla creada anteriormente, a partir de la consulta del punto 4:
- 
+    INSERT INTO INSCRITOSPORDEPORTEPORANIO SELECT D.NOMBRE,I.AO,COUNT(I.CODIGODEPORTE) AS CANTIDAD FROM DEPORTES AS D JOIN INSCRITOS AS I ON D.CODIGO=I.CODIGODEPORTE GROUP BY D.NOMBRE,I.AO;
 
 -- 8-Muestre el nombre del socio, el nombre del deporte y el año de inscripción de todas las inscripciones con cuota impaga:
- 
+    SELECT S.NOMBRE AS SOCIO,D.NOMBRE AS DEPORTE.AO FROM SOCIOS AS S JOIN INSCRITOS AS I ON S.DOCUMENTO=I.DOCUMENTO JOIN DEPORTES AS D ON I.CODIGODEPORTE=D.CODIGO WHERE I.CUOTA='N';
 
 -- 9-Elimine la tabla `sociosdeudores` si existe.
-
+    DROP TABLE IF EXISTS SOCIOSDEUDORES;
 
 -- 10-Cree la tabla:
- 
+    CREATE TABLE SOCIOSDEUDORES(
+        SOCIO VARCHAR(30);
+        DEPORTE VARCHAR(15);
+        AO YEAR;
+    )
 
 -- 11-Intente ingresar en la tabla anteriormente creada los registros resultado de la consulta del punto 8, colocando los campos `socio` y `deporte` en la lista de campos a cargar:
- 
+    SELECT S.NOMBRE,D.NOMBRE,I.AÑO FROM SOCIOS AS S JOIN INSCRITOS AS I ON S.DOCUMENTO=I.DOCUMENTO JOIN DEPORTE AS D ON I.CODIGODEPORTE=D.CODIGO WHERE I.CUOTA;
 
 -- 12-Ingrese en la tabla anteriormente creada los registros resultado de la consulta del punto 8, correctamente:
- 
+    INSERT INTO SOCIOSDEUDORES SELECT S.NOMBRE,D.NOMBRE,I.AÑO FROM SOCIOS AS S JOIN INSCRITOS AS I ON S.DOCUMENTO=I.DOCUMENTO JOIN DEPORTE AS D ON I.CODIGODEPORTE=D.CODIGO WHERE I.CUOTA;
 
 -- 13-Vea los registros de `sociosdeudores`.
-
+    SHOW TABLE SOCIOSDEUDORES;
 
 -- 14-Agrupe por nombre del profesor y deporte y cuente la cantidad de alumnos por profesor y deporte, considerando los deportes para los que no haya inscritos:
- 
+    SELECT D.PROFESOR,D.NOMBRE,COUNT(I.CODIGODEPORTE) FROM DEPORTES AS D LEFT JOIN INSCRITOS AS I ON I.CODIGODEPORTE=D.CODIGO GROUP BY D.PROFESOR,D.NOMBRE;
 
 -- 15-Elimine la tabla `alumnosporprofesor` si existe.
-
+    DROP TABLE IF EXISTS ALUMNOSPORPROFESOR;
 
 
 -- 16-Cree la tabla `alumnosporprofesor`:
- 
-
-
+    CREATE TABLE ALUMNOSPORPROFESOR(
+        PROFESOR VARCHAR(30);
+        DEPORTE VARCHAR(15);
+        CANTIDAD TINYINT UNSIGNED;
+    )
 
 -- 17-Ingrese en la tabla creada en el punto anterior con el resultado de la consulta del punto 14:
+    INSERT INTO ALUMNOSPORPROFESOR SELECT D.PROFESOR,D.NOMBRE,COUNT(I.CODIGODEPORTE) FROM DEPORTES AS D LEFT JOIN INSCRITOS AS I ON I.CODIGODEPORTE=D.CODIGO GROUP BY D.PROFESOR,D.NOMBRE;
  
-
 -- 18-Muestre todos los registros de la nueva tabla.
+    SHOW TABLE ALUMNOSPORPROFESOR;
